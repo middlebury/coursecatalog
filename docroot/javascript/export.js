54fdb3744ea95a9c6bd9c407a4d01c81bf865ab1
Corey Selover <gselover@middlebury.edu>
2018-01-08 11:25:00 -0500

    Remove section type switching. Begin JSON generation

--- docroot/javascript/export.js
+++ docroot/javascript/export.js
@@ -20,3 +20,36 @@ function updateSection(sectionId) {
   // put the old value into the new type if applicable.
 
 }
+
+function saveJSON() {
+
+  var JSONString = "{";
+  var sections = $('.section').toArray();
+
+  sections.forEach(function(element) {
+    var sectionAsDOMObject = $.parseHTML($(element).html());
+    //console.log(sectionAsDOMObject);
+    var sectionType = sectionAsDOMObject[0].innerHTML.substring(sectionAsDOMObject[0].innerHTML.indexOf(": ") + 2);
+    var sectionValueHTML = sectionAsDOMObject[1].innerHTML.substring(sectionAsDOMObject[1].innerHTML.indexOf(": ") + 2);
+    //console.log(sectionValueHTML);
+    // Extract the value based on sectionType.
+    switch(sectionType) {
+      case 'h1':
+      case 'h2':
+      case 'page_content':
+      case 'custom_text':
+        var sectionValue = sectionValueHTML.substring(sectionValueHTML.indexOf('value=') + 6, sectionValueHTML.indexOf('>'));
+        break;
+      case 'course_list':
+        var sectionValue = 'not yet';
+        break;
+      default:
+        throw 'Invalid section type: ' + sectionType;
+    }
+    console.log(sectionValue);
+  });
+
+  JSONString += "}";
+
+  console.log(JSONString);
+}
