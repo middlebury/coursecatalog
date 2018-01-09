0533dbe77f45548cc4ad44207b729741acc815ef
Corey Selover <gselover@middlebury.edu>
2018-01-09 10:32:21 -0500

    Generate course lists for adding sections

--- application/controllers/ExportController.php
+++ application/controllers/ExportController.php
@@ -32,7 +32,57 @@ class ExportController extends Zend_Controller_Action
   public function generatecourselistAction()
   {
     if ($this->_getParam('catalogId')) {
-      echo "<select class='section-dropdown' value='unselected'><option value='unselected' selected='selected'>Please choose a subject</option><optgroup label='Subjects'></optgroup><optgroup label='Departments'></optgroup></select>";
+      $catalogId = $this->_helper->osidId->fromString($this->_getParam('catalogId'));
+      $this->departmentType = new phpkit_type_URNInetType("urn:inet:middlebury.edu:genera:topic/department");
+			$this->subjectType = new phpkit_type_URNInetType("urn:inet:middlebury.edu:genera:topic/subject");
+
+      $topicSearchSession = $this->_helper->osid->getCourseManager()->getTopicSearchSessionForCatalog($catalogId);
+			$topicQuery = $topicSearchSession->getTopicQuery();
+			$topicQuery->matchGenusType($this->departmentType, true);
+			if (isset($termId) && $topicQuery->hasRecordType($this->termType)) {
+				$record = $topicQuery->getTopicQueryRecord($this->termType);
+				$record->matchTermId($termId, true);
+			}
+			$search = $topicSearchSession->getTopicSearch();
+			$order = $topicSearchSession->getTopicSearchOrder();
+			$order->orderByDisplayName();
+			$search->orderTopicResults($order);
+			$searchResults = $topicSearchSession->getTopicsBySearch($topicQuery, $search);
+			$departments = $searchResults->getTopics();
+
+			$topicQuery = $topicSearchSession->getTopicQuery();
+			$topicQuery->matchGenusType($this->subjectType, true);
+			if (isset($termId) && $topicQuery->hasRecordType($this->termType)) {
+				$record = $topicQuery->getTopicQueryRecord($this->termType);
+				$record->matchTermId($termId, true);
+			}
+			$search = $topicSearchSession->getTopicSearch();
+			$order = $topicSearchSession->getTopicSearchOrder();
+			$order->orderByDisplayName();
+			$search->orderTopicResults($order);
+			$searchResults = $topicSearchSession->getTopicsBySearch($topicQuery, $search);
+			$subjects = $searchResults->getTopics();
+
+      $sectionInput = "
+      <select class='section-dropdown' value='unselected'>
+        <option value='unselected'>Please choose a subject</option>
+        <optgroup label='Subjects'>";
+        while ($subjects->hasNext()) {
+          $topic = $subjects->getNextTopic();
+          $sectionInput .= "<option value='" . Zend_Controller_Action_HelperBroker::getStaticHelper('OsidId')->toString($topic->getId()) . "'>" . $this->view->escape($topic->getDisplayName()) . "</option>";
+        }
+        $sectionInput .= "
+        </optgroup>
+        <optgroup label='Departments'>";
+        while ($departments->hasNext()) {
+          $topic = $departments->getNextTopic();
+          $sectionInput .= "<option value='" . Zend_Controller_Action_HelperBroker::getStaticHelper('OsidId')->toString($topic->getId()) . "'>" . $this->view->escape($topic->getDisplayName()) . "</option>";
+        }
+        $sectionInput .= "
+        </optgroup>
+      </select>";
+
+      echo $sectionInput;
 
       $this->_helper->layout()->disableLayout();
       $this->_helper->viewRenderer->setNoRender(true);
