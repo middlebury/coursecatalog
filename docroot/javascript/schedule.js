88aeeae2cdef95a4106371234157b1ba83ac1c70
Corey Selover <gselover@middlebury.edu>
2018-01-17 12:35:51 -0500

    Fully display archive jobs

--- docroot/javascript/schedule.js
+++ docroot/javascript/schedule.js
@@ -1,38 +1,77 @@
 
+function defineConfigDropDown(configs) {
+  var configDropDownHTML = "<select class='config-dropdown' value='unselected'><option value='unselected' selected>Please select a config</option>";
+  configs.forEach(function(element) {
+    configDropDownHTML += "<option value='" + element['id'] + "'>" + element['label'] + "</option>";
+  });
+  configDropDownHTML += "</select>";
+  return configDropDownHTML;
+}
+
+function selectConfig(configId, configDropDown) {
+  configDropDown = configDropDown.replace(" selected", "");
+  configDropDown = configDropDown.replace("value=\'" + configId + "\'", "value=\'" + configId + "\' selected");
+  var currentValue = configDropDown.substring(configDropDown.indexOf("class=\'config-dropdown\' value=\'"), configDropDown.indexOf("\'><option"));
+  configDropDown = configDropDown.replace(currentValue, "class=\'config-dropdown\' value=\'" + configId);
+  return configDropDown;
+}
+
+function defineRevisionsDropDown(revisions) {
+  var revisionsDropDownHTML = "<select class='revision-dropdown' value='unselected'><option value='latest' selected>Latest</option>";
+  revisions.forEach(function(element) {
+    revisionsDropDownHTML += "<option value='" + element['id'] + "'>" + element['last_saved'] + "</option>";
+  });
+  revisionsDropDownHTML += "</select>";
+  return revisionsDropDownHTML;
+}
+
+function selectRevision(revisionId, revisionDropDown) {
+  revisionDropDown = revisionDropDown.replace(" selected", "");
+  revisionDropDown = revisionDropDown.replace("value=\'" + revisionId + "\'", "value=\'" + revisionId + "\' selected");
+  var currentValue = revisionDropDown.substring(revisionDropDown.indexOf("class=\'revision-dropdown\' value=\'"), revisionDropDown.indexOf("\'><option"));
+  revisionDropDown = revisionDropDown.replace(currentValue, "class=\'revisions-dropdown\' value=\'" + revisionId);
+  return revisionDropDown;
+}
+
+function actions() {
+  return "<button value='delete'>Delete</button><button value='run'>Run</button>";
+}
+
 function buildList(data, callback) {
-  console.log(data);
 
-  // Generate config dropdown
-  var configDropDownHTML = "";
-  // data[0][configs].foreach(function(element) {
-  //   configDropDownHTML +=
-  // });
+  //defineConfigDropDown(data[0]['configs']);
+  //defineRevisionsDropDown(data[1]['revisions']);
 
-  /*
-  var jobs = data.split("; ");
-
-  // This split creates an extra, blank job. So, delete it.
-  jobs.pop();
-  var jobHTML = "";
-  jobs.forEach(function(element) {
-    var values = element.split(", ");
+  var jobsHTML = "";
+  data[2]['jobs'].forEach(function(element) {
 
     // ID && Active
-    jobHTML += "<tr><td><input type='hidden' value='" + values[0] + "'></input><input type='checkbox'";
-    if (values[1] === '1') jobHTML += " checked";
-    jobHTML += "></input></td>";
+    jobsHTML += "<tr><td><input type='hidden' value='" + element['id'] + "'></input><input type='checkbox'";
+    if (element['active'] === '1') jobsHTML += " checked";
+    jobsHTML += "></input></td>";
 
     // Export Path
-    jobHTML += "<td><input value='" + values[2] + "'></input></td>";
+    jobsHTML += "<td><input value='" + element['export_path'] + "'></input></td>";
 
     // Config
-    jobHTML += "<td><select><option>test</option></select></td>";
+    var configDropDownHTML = selectConfig(element['config_id'], defineConfigDropDown(data[0]['configs']));
+    jobsHTML += "<td>" + configDropDownHTML + "</td>";
 
-    jobHTML += "</tr>";
+    // Revisions
+    var validRevisions = data[1]['revisions'].filter(function(revision) { return revision['arch_conf_id'] === element['config_id']; });
+    var revisionsDropDownHTML = selectRevision(element['revision_id'], defineRevisionsDropDown(validRevisions));
+    jobsHTML += "<td>" + revisionsDropDownHTML + "</td>";
+
+    // Terms
+    jobsHTML += "<td><input value='" + element['terms'] + "'></input></td>";
+
+    // Actions
+    jobsHTML += "<td>" + actions() + "</td>";
+    jobsHTML += "</tr>";
   });
 
-  $('#job-table').append(jobHTML);
-  */
+  $('#job-table').append(jobsHTML);
+
   callback();
 }
 
