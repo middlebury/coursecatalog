52f8828e9800a09846647cc25347eb9bbbdc8b64
Corey Selover <gselover@middlebury.edu>
2018-01-25 13:22:59 -0500

    Improve diff formatting

--- docroot/javascript/revision_history.js
+++ docroot/javascript/revision_history.js
@@ -3,14 +3,58 @@ var selected = [];
 
 function prettifyDiff(diff) {
   var output = "<pre class='diff'>";
-  diff.forEach(function(element) {
+  diff.forEach(function(element, index) {
     if(element[0] === 1) {
-      output += "<div class='added'>" + element[1] + "</div>";
+      if(index > 0 && diff[index - 1][0] === 0) {
+        if (diff[index - 1][1].indexOf("group") < 5) {}
+        output += "...\n" + diff[index - 1][1].substring(diff[index - 1][1].lastIndexOf("group") - 2);
+      }
+      output += "<span class='added'>" + element[1] + "</span>";
+      if(diff[index + 1] && diff[index + 1][0] === 0) {
+        output += diff[index + 1][1].substring(0, Math.max(diff[index + 1][1].indexOf("group"), 10));
+      }
     } else if (element[0] === -1) {
-      output += "<div class='removed'>" + element[1] + "</div>";
+      if(index > 0 && diff[index - 1][0] === 0) {
+        output += diff[index - 1][1].substring(diff[index - 1][1].lastIndexOf("group") - 2);
+      }
+      output += "<span class='removed'>" + element[1] + "</span>";
+      if(diff[index + 1] && diff[index + 1][0] === 0) {
+        output += diff[index + 1][1].substring(0, Math.max(diff[index + 1][1].indexOf("group"), 10));
+      }
+    } else {
+      //output += element[1];
     }
   });
   output += "</pre>";
+
+  // // Remove groups with no changes.
+  // var groups = [];
+  // var spans = [];
+  // var groupReg = /group/gi;
+  // var spanReg = /<span/gi;
+  // var result;
+  // while ( (result = groupReg.exec(output)) ) {
+  //   groups.push(result.index);
+  // }
+  // while ( (result = spanReg.exec(output)) ) {
+  //   spans.push(result.index);
+  // }
+  //
+  // var validGroups = [];
+  // groups.forEach(function(group, groupIndex) {
+  //   spans.forEach(function(span, spanIndex) {
+  //     if(span > group && ( span < groups[groupIndex + 1] || groupIndex === groups.length - 1) ) {
+  //       validGroups.push(group);
+  //     }
+  //   });
+  // });
+  //
+  // // Remove duplicate groups. Taken from https://stackoverflow.com/questions/9229645/remove-duplicate-values-from-js-array
+  // validGroups = validGroups.reduce(function(a,b){if(a.indexOf(b)<0)a.push(b);return a;},[]);
+  // validGroups.foreach(function(element) {
+  //
+  // });
+
   return output;
 }
 
@@ -40,6 +84,8 @@ function compare() {
   var text2 = $('#' + selected[1]).parents('tr').find('.json-data')[0].innerText;
   var diff = dmp.diff_main(text1, text2);
   dmp.diff_cleanupSemantic(diff);
+
+  console.log(diff);
 
   $(comparison)[0].innerHTML = prettifyDiff(diff);
   $(comparison).removeClass('hidden');
