7252f91202185a668f1262c6c5e8ccf6efe386b4
Corey Selover <gselover@middlebury.edu>
2018-01-30 16:26:53 -0500

    Fix typo

--- docroot/javascript/export.js
+++ docroot/javascript/export.js
@@ -204,7 +204,7 @@ function validateInput(id, type, value, callback) {
       //   callback();
       // } else {
       //   callback("Headers may only contain letters, numbers, .,?!\&;:(), and double quotes (\"\").", id);
-      }
+      //}
       break;
     case 'page_content':
       var validURL = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/;
