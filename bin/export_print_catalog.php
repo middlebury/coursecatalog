dabef16055bd44e283434e23305c1b517cf45a13
Adam Franco <afranco@middlebury.edu>
2019-04-12 20:31:31 -0400

    Export: capture stderr and pass up to parent shell for visibility.

--- bin/export_print_catalog.php
+++ bin/export_print_catalog.php
@@ -21,11 +21,29 @@ $base = '';
 if (!empty($config->catalog->archive->url_base)) {
 	$base = '-b '.escapeshellarg($config->catalog->archive->url_base);
 }
-exec(BASE_PATH.'/bin/zfcli.php '.$base.' -a archive.export_active_jobs -p '.escapeshellarg(http_build_query($params)), $output, $return);
+$return = shell_exec_with_pipes(BASE_PATH.'/bin/zfcli.php '.$base.' -a archive.export_active_jobs -p '.escapeshellarg(http_build_query($params)), $stdout, $stderr);
+if (strlen($stdout)) {
+	print $stdout;
+}
 if ($return) {
 	file_put_contents('php://stderr', "Error running command:\n\n\tarchive.export_active_jobs\n");
-	file_put_contents('php://stderr', "$return\n");
-	return 2;
+	file_put_contents('php://stderr', "$stderr\n");
+	return $return;
+} elseif (strlen($stderr)) {
+	file_put_contents('php://stderr', $stderr);
 }
 // Success
 return 0;
+
+
+function shell_exec_with_pipes($cmd, &$stdout=null, &$stderr=null) {
+	$proc = proc_open($cmd,[
+		1 => ['pipe','w'],
+		2 => ['pipe','w'],
+	],$pipes);
+	$stdout = stream_get_contents($pipes[1]);
+	fclose($pipes[1]);
+	$stderr = stream_get_contents($pipes[2]);
+	fclose($pipes[2]);
+	return proc_close($proc);
+}
