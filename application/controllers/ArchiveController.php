292a476b2ea5d55ba97e8e6809d3b7229ec6fb77
Adam Franco <afranco@middlebury.edu>
2022-03-18 11:54:53 -0400

    Fix undefined $text variable.

--- application/controllers/ArchiveController.php
+++ application/controllers/ArchiveController.php
@@ -560,15 +560,15 @@
 		$totalSections = count($sections);
 		$currentSection = 1;
 		foreach ($this->view->sections as $key => &$section) {
+			$text = '';
+			if (!empty($section['text'])) {
+				$text = $section['text'];
+			} else if (!empty($section['url'])) {
+				$text = $section['url'];
+			}else if (!empty($section['id'])) {
+				$text = $this->_helper->osidId->toString($section['id']);
+			}
 			if ($request->getParam('verbose')) {
-				$text = '';
-				if (!empty($section['text'])) {
-					$text = $section['text'];
-				} else if (!empty($section['url'])) {
-					$text = $section['url'];
-				}else if (!empty($section['id'])) {
-					$text = $this->_helper->osidId->toString($section['id']);
-				}
 				file_put_contents('php://stderr',str_pad($section['type'].': ', 15, ' ', STR_PAD_RIGHT).$text."\n");
 			}
 			$progressUpdateStmt->execute([
