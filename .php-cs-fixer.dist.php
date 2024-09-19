533db87b0c6b903cc6af7c1513ae5da66028d055
Adam Franco <afranco@middlebury.edu>
2024-09-19 15:22:24 -0400

    PhpCsFixer: Use trailing commas in multiline statements.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -12,6 +12,7 @@ return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
         // '@Symfony:risky' => true,
+        'trailing_comma_in_multiline' => ['elements' => ['arrays', 'match', 'parameters']],
     ])
     ->setFinder($finder)
 ;
