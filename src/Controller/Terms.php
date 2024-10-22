62ba29a1be4392c7164d23820a4f9f6d86198644
Adam Franco <afranco@middlebury.edu>
2024-10-22 16:30:40 -0400

    #47: Port /terms/view/* action.

--- src/Controller/Terms.php
+++ src/Controller/Terms.php
@@ -6,6 +6,7 @@
 
 namespace App\Controller;
 
+use App\Service\Osid\DataLoader;
 use App\Service\Osid\IdMap;
 use App\Service\Osid\Runtime;
 use App\Service\Osid\TermHelper;
@@ -33,16 +34,24 @@ class Terms extends AbstractController
     private $osidIdMap;
 
     /**
+     * @var \App\Service\Osid\DataLoader
+     */
+    private $osidDataLoader;
+
+    /**
      * Construct a new Catalogs controller.
      *
      * @param \App\Service\Osid\Runtime $osidRuntime
      *   The osid.runtime service.
      * @param \App\Service\Osid\IdMap $osidIdMap
      *   The osid.id_map service.
+     * @param \App\Service\Osid\DataLoader $osidDataLoader
+     *   The data loader service.
      */
-    public function __construct(Runtime $osidRuntime, IdMap $osidIdMap) {
+    public function __construct(Runtime $osidRuntime, IdMap $osidIdMap, DataLoader $osidDataLoader) {
         $this->osidRuntime = $osidRuntime;
         $this->osidIdMap = $osidIdMap;
+        $this->osidDataLoader = $osidDataLoader;
     }
 
     /**
@@ -85,30 +94,32 @@ class Terms extends AbstractController
         $this->listAction();
     }
 
-    #[Route('/terms/view/{id}', name: 'view_term')]
-    public function viewAction($id)
+    #[Route('/terms/view/{term}/{catalog}', name: 'view_term')]
+    public function viewAction($term, $catalog = NULL)
     {
-        $id = $this->osidIdMap->fromString($this->_getParam('term'));
-        $lookupSession = $this->osidRuntime->getCourseManager()->getTermLookupSession();
-        $lookupSession->useFederatedCourseCatalogView();
-        $this->view->term = $lookupSession->getTerm($id);
+        $data = [];
+        $id = $this->osidIdMap->fromString($term);
 
-        $lookupSession = $this->osidRuntime->getCourseManager()->getCourseOfferingLookupSession();
-        $lookupSession->useFederatedCourseCatalogView();
-        $this->view->offerings = $lookupSession->getCourseOfferingsByTerm($id);
-
-        // Set the selected Catalog Id.
-        $catalogSession = $this->osidRuntime->getCourseManager()->getTermCatalogSession();
-        $catalogIds = $catalogSession->getCatalogIdsByTerm($id);
-        if ($catalogIds->hasNext()) {
-            $this->setSelectedCatalogId($catalogIds->getNextId());
+        if ($catalog) {
+            $catalogId = $this->osidIdMap->fromString($catalog);
+            $termLookupSession = $this->osidRuntime->getCourseManager()->getTermLookupSessionForCatalog($catalogId);
+            $termLookupSession->useIsolatedCourseCatalogView();
+            $offeringLookupSession = $this->osidRuntime->getCourseManager()->getCourseOfferingLookupSessionForCatalog($catalogId);
+            $offeringLookupSession->useIsolatedCourseCatalogView();
+        } else {
+            $termLookupSession = $this->osidRuntime->getCourseManager()->getTermLookupSession();
+            $termLookupSession->useFederatedCourseCatalogView();
+            $offeringLookupSession = $this->osidRuntime->getCourseManager()->getCourseOfferingLookupSession();
+            $offeringLookupSession->useFederatedCourseCatalogView();
+        }
+        $data['term'] = $termLookupSession->getTerm($id);
+        $offerings = $offeringLookupSession->getCourseOfferingsByTerm($id);
+        $data['offerings'] = [];
+        while ($offerings->hasNext()) {
+            $data['offerings'][] = $this->osidDataLoader->getOfferingData($offerings->getNextCourseOffering());
         }
 
-        // Set the title
-        $this->view->title = $this->view->term->getDisplayName();
-        $this->view->headTitle($this->view->title);
-
-        $this->view->menuIsTerms = true;
+        return $this->render('terms/view.html.twig', $data);
     }
 
     /**
