0bc37bcd03d6e9dd8b4aa29bc91df10a725ca4fa
Adam Franco <afranco@middlebury.edu>
2024-10-29 11:13:13 -0400

    #47: Redirect blind requests to /favicon.ico to the asset path.

---
+++ src/Controller/Redirect.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\AssetMapper\AssetMapperInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+/**
+ * A controller for application path redirects.
+ */
+class Redirect extends AbstractController
+{
+
+    /**
+     * Construct a new Redirect controller.
+     *
+     * @param \Symfony\Component\AssetMapper\AssetMapperInterface $assetMapper
+     *   The asset mapper service.
+     */
+    public function __construct(
+        private AssetMapperInterface $assetMapper,
+    ) { }
+
+    /**
+     * Redirect blind requests to /favicon.ico to our asset path.
+     *
+     * The HTML should be generating correct links to our hashed asset URLs,
+     * but some clients will blindly request the /favicon.ico path without
+     * looking at the HTML, so provide a fallback.
+     */
+    #[Route('/favicon.ico', name: 'favicon')]
+    public function favicon()
+    {
+        return $this->redirect($this->assetMapper->getPublicPath('favicon.ico'));
+    }
+}
