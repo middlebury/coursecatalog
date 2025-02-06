8e230f63a498f8ff98540a0152d76cecee879111
Adam Franco <afranco@middlebury.edu>
2025-02-06 13:48:07 -0500

    #47: Move Export process monitoring to a new service/listern class.
    This code had been in AdminExporter::generate() until it was pulled out in
    c3b6b860d7263a48a59a57e146cd6c3af6b59929.

--- src/Controller/AdminExporter.php
+++ src/Controller/AdminExporter.php
@@ -2,13 +2,8 @@
 
 namespace App\Controller;
 
-/*
- * @since 8/23/17
- *
- * @copyright Copyright &copy; 2017, Middlebury College
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
- */
-
+use App\Archive\Export\EventListener\ExportProcessMonitor;
+use App\Archive\ExportJob\ExportJobStorage;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
@@ -21,28 +16,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
  */
 class AdminExporter extends AbstractController
 {
-    /**
-     * Initialize object.
-     *
-     * Called from {@link __construct()} as final step of object instantiation.
-     *
-     * @return void
-     */
-    // public function init()
-    // {
-    //     $this->alternateType = new phpkit_type_URNInetType('urn:inet:middlebury.edu:record:alternates');
-    //     $this->alternateInTermsType = new phpkit_type_URNInetType('urn:inet:middlebury.edu:record:alternates-in-terms');
-    //     $this->identifiersType = new phpkit_type_URNInetType('urn:inet:middlebury.edu:record:banner_identifiers');
-    //
-    //     parent::init();
-    //     $this->_helper->layout()->setLayout('midd_archive');
-    //
-    //     // Store an HTTP client configuration for later use.
-    //     $this->httpClientConfig = [
-    //         'maxredirects' => 10,
-    //         'timeout' => 30,
-    //     ];
-    // }
+    public function __construct(
+        private ExportProcessMonitor $exportProcessMonitor,
+        private ExportJobStorage $exportJobStorage,
+    ) {
+    }
 
     /**
      * Export a single archive export job.
@@ -97,18 +75,7 @@ class AdminExporter extends AbstractController
      */
     public function jobprogressAction()
     {
-        $db = Zend_Registry::get('db');
-        $query =
-        'SELECT
-			progress
-		 FROM archive_export_progress';
-        $stmt = $db->prepare($query);
-        $stmt->execute();
-        $progress = $stmt->fetch();
-        echo $progress['progress'];
-
-        $this->_helper->layout()->disableLayout();
-        $this->_helper->viewRenderer->setNoRender(true);
+        return new JsonResponse($this->exportProcessMonitor->getAllProcesses());
     }
 
     /**
@@ -118,50 +85,9 @@ class AdminExporter extends AbstractController
      *
      * @since 1/26/18
      */
-    public function exportsinglejobAction()
+    public function exportsinglejobAction(int $jobId)
     {
-        $request = $this->getRequest();
-        if (!$request->getParam('id')) {
-            header('HTTP/1.1 400 Bad Request');
-            echo 'A job id must be specified.';
-            exit;
-        }
-
-        $this->_helper->layout()->disableLayout();
-        $this->_helper->viewRenderer->setNoRender(true);
-
-        $db = Zend_Registry::get('db');
-        $query =
-        'SELECT
-			*
-		 FROM archive_jobs
-		 WHERE id = ?';
-        $stmt = $db->prepare($query);
-        $stmt->execute([$request->getParam('id')]);
-        $job = $stmt->fetch();
-
-        // Revision is set to 'latest'
-        if (!$job['revision_id']) {
-            $query =
-            'SELECT
-				id
-			 FROM archive_configuration_revisions a
-			 INNER JOIN (
-				SELECT
-					arch_conf_id,
-					MAX(last_saved) as latest
-				FROM archive_configuration_revisions
-				GROUP BY arch_conf_id
-			) b ON a.arch_conf_id = b.arch_conf_id and a.last_saved = b.latest
-			 WHERE a.arch_conf_id = ?';
-            $stmt = $db->prepare($query);
-            $stmt->execute([$job['config_id']]);
-            $latestRevision = $stmt->fetch();
-            $job['revision_id'] = $latestRevision['id'];
-        }
-
-        $job['terms'] = explode(',', $job['terms']);
-        array_walk($job['terms'], function (&$value, $key) { $value = 'term/'.$value; });
+        $job = $this->exportJobStorage->getJob($jobId);
 
         $this->_helper->exportJob($job['export_path'], $job['config_id'], $job['terms'], $job['revision_id']);
     }
@@ -175,37 +101,7 @@ class AdminExporter extends AbstractController
      */
     public function exportactivejobsAction()
     {
-        $this->_helper->layout()->disableLayout();
-        $this->_helper->viewRenderer->setNoRender(true);
-
-        $request = $this->getRequest();
-
-        if ($request->getParam('verbose')) {
-            $verbose = '1';
-        } else {
-            $verbose = '0';
-        }
-
-        $db = Zend_Registry::get('db');
-        $jobs = $db->query('SELECT * FROM archive_jobs WHERE active=1')->fetchAll();
-
-        foreach ($jobs as $job) {
-            $terms = explode(',', $job['terms']);
-            foreach ($terms as &$term) {
-                $term = 'term/'.$term;
-            }
-            unset($term);
-
-            if (null === $job['revision_id']) {
-                $revision = 'latest';
-            } else {
-                $revision = $job['revision_id'];
-            }
-
-            if ($verbose) {
-                file_put_contents('php://stderr', 'Beginning export: '.$job['config_id'].' '.$job['export_path'].' '.$job['terms'].' '.$revision."\n");
-            }
-
+        foreach ($this->exportJobStorage->getActiveJobs() as $job) {
             $this->_helper->exportJob($job['export_path'], $job['config_id'], $terms, $revision, $verbose);
         }
     }
